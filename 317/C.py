42b2b1b2b1405593f5bbf29b5b6e73f0a7ec7a68
2nagatomo2 <2nagatomo2@gmail.com>
2023-12-06 15:06:10 +0900

    C.py(AC)

--- 317/C.py
+++ 317/C.py
@@ -1,25 +1,28 @@
-import itertools
-
+# dfs(深さ優先探索)を使用して実装
+# CPythonでは通らなかったがPyPyでは通った
+#この二つの違い何？？
 N, M = map(int, input().split())
-tmp = [i for i in range(1, N+1)]
-loads = list(itertools.permutations(tmp))
-G = [[0 for _ in range(N+1)] for _ in range(N+1)]
 
-for i in range(M):
-    a, b, c = map(int, input().split())
+G = [[0] * (N+1) for _ in range(N+1)]
+
+for _ in range(M):
+    a, b , c = map(int, input().split())
     G[a][b] = c
     G[b][a] = c
-ans = 0
 
-for i in loads:
-    dist = 0
-    for j in range(N-1):
-        start = i[j]
-        end = i[j+1]
-        if G[start][end] != 0:
-            dist += G[start][end]
-        else:
-            break
+ans = 0
+visited = [False] * (N+1)
+
+def dfs(pos, dist):
+    global ans
+    visited[pos] = True
     ans = max(ans, dist)
+    for i in range(1, N+1):
+        if not visited[i] and G[pos][i] != 0:
+            dfs(i, dist+G[pos][i])
+    visited[pos] = False
+
+for i in range(1, N+1):
+    dfs(i, 0)
 
 print(ans)
