18ef790d014add37d4503c1a760a18a521b17bba
2nagatomo2 <2nagatomo2@gmail.com>
2024-01-09 10:57:02 +0900

    334 C問題

---
+++ 334/c.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+
+#define rep(i,n) for (int i=0; i<(n); i++)
+
+int main() {
+    int a = -2 / 3;
+    cout << a << endl;
+    return 0;
+}
