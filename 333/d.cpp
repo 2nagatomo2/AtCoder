becbe0f5b7f0ef272a1d16af76f3ae244960431c
2nagatomo2 <2nagatomo2@gmail.com>
2024-01-10 16:27:51 +0900

    333 D問題

--- 333/d.cpp
+++ 333/d.cpp
@@ -6,40 +6,66 @@ using namespace std;
 
 #define rep(i,n) for (int i=0; i<(n); i++)
 
+struct UnionFind {
+    int par[1000000];
+    int siz[1000000];
+
+    // N 頂点の Union-Find を作成
+    void init(int N) {
+        for(int i = 1; i <= N; i++) par[i] = -1; // 最初は親がいない
+        for(int i = 1; i <= N; i++) siz[i] = 1;  // 最初はグループの超点数が1
+    }
+
+    // 頂点 x の根を返す関数
+    int root(int x) {
+        while(true) {
+            if(par[x] == -1) break; // 1個先（親）がなければ、ここが根
+            x = par[x];             // 1個先（親）に進む
+        }
+        return x;
+    }
+
+    // 頂点 u と v を統合する関数
+    void unite(int u, int v) {
+        int RootU = root(u);
+        int RootV = root(v);
+        if(RootU == RootV) return; // u と v が同じグループの時は処理を行わない
+        if(siz[RootU] < siz[RootV]) {
+            par[RootU] = RootV;
+            siz[RootV] += siz[RootU];
+        }
+        else {
+            par[RootV] = RootU;
+            siz[RootU] += siz[RootV];
+        }
+    }
+
+    // 要素 u と v が同一のグループかどうかを返す関数
+    bool same(int u, int v) {
+        if(root(u) == root(v)) return true;
+        else return false;
+    }
+};
+
 int main() {
     int n;
     cin >> n;
-    vector<vector<int> > g(n);
-    vector<bool> visited(n, false);
-    int v1, v2;
-    vector<set<int> > next(n);
+    UnionFind UF;
+    UF.init(n);
+
+    int u, v;
     rep(i, n-1) {
-        cin >> v1 >> v2;
-        v1--; v2--;
-        g[v1].push_back(v2);
-        g[v2].push_back(v1);
-        if(v1 == 0) {
-            next[v2].insert(v2);
-        }
-    }
-    
-
-    int ans = 1000000, cost = 0, cnt = 0;
-    stack<int> st;
-    st.push(0);
-    visited[0] = true;
-    while(!st.empty()) {
-        int pos = st.top();
-        st.pop();
-        for(int v : g[pos]) {
-            if(!visited[v]) {
-                visited[v] = true;
-                st.push(v);
-                cnt++;
-            }
+        cin >> u >> v;
+        if(u != 1 && v != 1) {
+            UF.unite(u, v);
         }
     }
 
-    cout << ans << endl;    
+    int max_size = 0;
+    for(int i = 2; i <= n; i++) {
+        max_size = max(max_size, UF.siz[i]);
+    }
+    int ans = n - max_size;
+    cout << ans << endl;
     return 0;
 }
