d03ddc407012a4cb56a3403a1335114b96558575
2nagatomo2 <2nagatomo2@gmail.com>
2024-02-07 05:19:59 +0900

    319 D問題 未完成

---
+++ 319/d.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+
+#define rep(i,n) for (int i=0; i<(n); i++)
+
+int main() {
+    int n, m;
+    cin >> n, m;
+    ll l[n];
+    rep(i, n) cin >> l[i];
+
+    ll w[m];
+    return 0;
+}
