5a9ecb504fcadd491913c7f793e34c963692f6f9
2nagatomo2 <2nagatomo2@gmail.com>
2024-02-07 05:18:52 +0900

    291 D問題

---
+++ 291/d.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+
+#define rep(i,n) for (int i=0; i<(n); i++)
+
+int main() {
+    int n;
+    cin >> n;
+    ll mod = 998244353;
+    ll a[n], b[n];
+    vector<vector<ll> > g(n);
+    rep(i, n) {
+        cin >> a[i] >> b[i];
+
+    }
+    return 0;
+}
