1cf3b569100204e1da392de6c4fdd7743b3005d1
2nagatomo2 <2nagatomo2@gmail.com>
2024-02-07 05:21:15 +0900

    327 D問題 未解決

--- 327/d.cpp
+++ 327/d.cpp
@@ -8,6 +8,24 @@ using namespace std;
 
 #define rep(i,n) for (int i=0; i<(n); i++)
 
-int main() {
+void dfs(int pos, int x, vector<vector<int> >& g) {
+    
+}
 
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(n);
+    vector<vector<int> > g(n);
+    rep(i, m) {
+        cin >> a[i] >>b[i];
+        a[i]--; b[i]--;
+        g[a[i]].push_back(b[i]);
+        g[b[i]].push_back(a[i]);
+    }
+
+    vector<int> x(n, -1);
+    rep(i, n) {
+        if(x[i] == -1) dfs(i, 0, g);
+    }
 }
