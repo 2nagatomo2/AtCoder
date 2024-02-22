acebf1f0bec2972d7dfccf7b2c76fb9b9886c8b0
2nagatomo2 <2nagatomo2@gmail.com>
2024-02-22 15:23:05 +0900

    336 D問題

--- 336/d.cpp
+++ 336/d.cpp
@@ -11,9 +11,27 @@ int main() {
     cin >> n;
     int a[n];
     rep(i, n) cin >> a[i];
-    int max_k = (n - 1) / 2;
-    rep(i, max_k) {
-        
+    int p_l[n], p_r[n];
+    rep(i, n) {
+        p_l[i] = 0;
+        p_r[i] = 0;
     }
+
+    if(a[0] == 0) p_l[0] = 0;
+    else p_l[0] = 1;
+    if(a[n-1] == 0) p_r[n-1] = 0;
+    else p_r[n-1] = 1;
+    rep(i, n-1) {
+        if(a[i+1] < i+2) p_l[i+1] = min(p_l[i]+1, a[i+1]);
+        else p_l[i+1] = min(i+2, p_l[i]+1);
+
+        if(a[n-i-2] < i+2) p_r[n-i-2] = min(p_r[n-i-1]+1, a[n-i-2]);
+        else p_r[n-i-2] = min(i+2, p_r[n-i-1]+1);
+    }
+    int ans = 0;
+    rep(i, n) {
+        ans = max(ans, min(p_l[i], p_r[i]));
+    }
+    cout << ans << endl;
     return 0;
 }
