0db0de2bc7d123550041936791ac1e1c7e949d1e
2nagatomo2 <2nagatomo2@gmail.com>
2024-02-22 15:21:09 +0900

    327 D問題 README.md

--- 327/d.cpp
+++ 327/d.cpp
@@ -1,31 +1,64 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 #define ull unsigned long long
 #define ll long long
 
 #define rep(i,n) for (int i=0; i<(n); i++)
-
-void dfs(int pos, int x, vector<vector<int> >& g) {
-    
-}
+#define REP(i,b,n) for(int i=b; i<(n); i++)
 
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<int> a(n), b(n);
-    vector<vector<int> > g(n);
+    int a[m], b[m], x[n];
     rep(i, m) {
-        cin >> a[i] >>b[i];
-        a[i]--; b[i]--;
+        cin >> a[i];
+        a[i]--;
+        }
+    rep(i, m) {
+        cin >> b[i];
+        b[i]--;
+    }
+    rep(i, n) x[i] = -1;
+    vector<vector<int>> g(n);
+    rep(i, m) {
         g[a[i]].push_back(b[i]);
         g[b[i]].push_back(a[i]);
     }
 
-    vector<int> x(n, -1);
+    vector<bool> visited(n, false);
+    stack<int> st; 
     rep(i, n) {
-        if(x[i] == -1) dfs(i, 0, g);
+        if(visited[i]) continue;
+        else {
+            st.push(i);
+            x[i] = 0;
+            while(!st.empty()) {
+                int pos = st.top();
+                st.pop();
+                if(!visited[pos]) {
+                    visited[pos] = true;
+                    for(int v : g[pos]) {
+                        if(x[pos] == 0) {
+                            if(x[v] == 0) {
+                                cout << "No" << endl;
+                                return 0;
+                            }
+                            else x[v] = 1;
+                        }
+                        else {
+                            if(x[v] == 1) {
+                                cout << "No" << endl;
+                                return 0;
+                            } 
+                            else x[v] = 0;
+                        }
+                        st.push(v);
+                    }
+                }
+            }
+        }
     }
+    cout << "Yes" << endl;
+    return 0;
 }
