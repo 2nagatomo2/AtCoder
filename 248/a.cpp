17c2339302b84feffd711afcf6e42a6747fc48da
2nagatomo2 <2nagatomo2@gmail.com>
2024-03-27 14:26:37 +0900

    248

---
+++ 248/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define REP(i,b,n) for(int i=b; i<(n); i++)
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+
+int main() {
+    string s;
+    cin >> s;
+    int ans = 45;
+    rep(i, 9) ans -= int(s[i] - '0');
+    cout << ans << endl;
+    return 0;
+}
---
+++ 248/b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define REP(i,b,n) for(int i=b; i<(n); i++)
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+
+int main() {
+    ll a, b, k;
+    cin >> a >> b >> k;
+    ll s = a, ans = 0;
+    while(s < b) {
+        s *= k;
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
