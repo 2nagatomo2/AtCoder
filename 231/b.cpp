8ac6931715893d2eb85f5b9b800cff5516088f31
2nagatomo2 <2nagatomo2@gmail.com>
2024-04-12 09:46:24 +0900

    231

---
+++ 231/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define REP(i,b,n) for(int i=b; i<(n); i++)
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+
+int main() {
+    int n;
+    cin >> n;
+    string s[n];
+    map<string, int> mp;
+    rep(i, n) {
+        cin >> s[i];
+        mp[s[i]]++;
+    }
+    string ans = "";
+    for(auto itr = mp.begin(); itr != mp.end(); itr++) {
+        if(mp[ans] < itr->second) {
+            ans = itr->first;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
