b4ffa752beef9f9ee8251b46332310133bea9556
2nagatomo2 <2nagatomo2@gmail.com>
2024-07-19 15:24:40 +0900

    add 191 A~B

---
+++ 191/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define REP(i,b,n) for(int i=b; i<(n); i++)
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+
+int main() {
+    int n, x, a;
+    cin >> n >> x;
+    rep(i, n) {
+        cin >> a;
+        if(x != a) cout << a << ' ';
+    }
+    cout << endl;
+    return 0;
+}
