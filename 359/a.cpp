10df4f6ebc6ce9227c972c36b790aa7479ca1dd3
2nagatomo2 <2nagatomo2@gmail.com>
2024-07-25 17:18:18 +0900

    add 359 A

--- 359/a.cpp
+++ 359/a.cpp
@@ -13,11 +13,14 @@ using namespace std;
 #define pii pair<int, int>
 
 int main() {
-    int a, b;
-    cin >> a >> b;
-    if(a+b >= 15 && b >= 8) cout << 1 << endl;
-    else if(a+b >= 10 && b >= 3) cout << 2 << endl;
-    else if(a+b >= 3) cout << 3 << endl;
-    else cout << 4 << endl;
+    int n;
+    cin >> n;
+    string s;
+    int ans = 0;
+    rep(i, n) {
+        cin >> s;
+        if(s == "Takahashi") ans++;
+    }
+    cout << ans << endl;
     return 0;
 }
