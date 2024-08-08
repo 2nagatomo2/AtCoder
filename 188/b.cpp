04ba7a277fc4c4aa34a0075a34286a63376408f7
2nagatomo2 <2nagatomo2@gmail.com>
2024-08-08 14:06:16 +0900

    add 189 A~B

---
+++ 188/b.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+
+#define rep(i,n) for (int i=0; i<n; i++)
+#define REP(i,b,n) for(int i=b; i<n; i++)
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+
+    ll t = 0;
+    rep(i, n) t += a[i] * b[i];
+    if(t == 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
